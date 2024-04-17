bd5f977e38b95956fb854f01db3e5aa46cb7948d
divyayush-v-20 <122596172+divyayush-v-20@users.noreply.github.com>
2024-04-17 22:06:03 +0530

    Add files via upload

---
+++ Sneaker Head/src/components/tagline.jsx
@@ -0,0 +1,15 @@
+function Tagline(){
+    return(
+        <>
+            <main>
+                <div className="b1">
+                    
+                </div>
+                <div className="b2">
+
+                </div>
+            </main>
+        </>
+    )
+}
+export default Tagline
